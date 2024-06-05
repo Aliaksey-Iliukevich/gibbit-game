411de6ce4749ed918e2ce4dee1257915b7d88630
Alexey Iliukevich <aleks.ilykevich@gmail.com>
2024-06-05 19:35:41 +0300

    feat: app done

---
+++ src/components/GameWrongLetters.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-if="wrongLetters" class="wrong-letters-container">
+        <div class="wrong-letters">
+        <p>Ошибки</p>
+        <span 
+            v-for="(letter, index) in wrongLetters"
+            :key="letter"
+        >
+            {{ index === wrongLetters.length -1 ? letter : `${letter}, `}}
+        </span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+interface Props {
+    wrongLetters: string[]
+}
+
+defineProps<Props>()
+</script>
